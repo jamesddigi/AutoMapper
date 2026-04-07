0b288c3e4f05a5b6c950022794f7f0b02ffdebac
James Dickinson <jamesd@digiterre.com>
2026-04-07 14:49:27 +0100

    Adding copy folder

---
+++ src/AutoMapper copy/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: NeutralResourcesLanguage("en")]
+
+namespace System.Runtime.CompilerServices;
+
+static class IsExternalInit { }
---
+++ src/AutoMapper copy/QueryableExtensions/ProjectionMappers/NullableSourceProjectionMapper.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoMapper.QueryableExtensions.Impl;
+
+public sealed class NullableSourceProjectionMapper : IProjectionMapper
+{
+    public Expression Project(IGlobalConfiguration configuration, in ProjectionRequest request, Expression resolvedSource, LetPropertyMaps letPropertyMaps) =>
+        Coalesce(resolvedSource, New(request.DestinationType));
+    public bool IsMatch(TypePair context) =>
+        context.DestinationType.IsValueType && !context.DestinationType.IsNullableType() && context.SourceType.IsNullableType();
+}
